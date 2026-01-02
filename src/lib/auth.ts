a3b5e6e9767139ed12ce2ea37368e0a07aea7967
chenyonghao <chenyonghao6717@yaocheng.cn>
2026-01-02 17:45:03 +0800

    enable better auth admin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/db";
 import { nextCookies } from "better-auth/next-js";
 import { headers } from "next/headers";
-import { organization } from "better-auth/plugins";
+import { organization, admin } from "better-auth/plugins";
 import { redirect, RedirectType } from "next/navigation";
 
 export const auth = betterAuth({
@@ -13,7 +13,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [organization(), nextCookies()], // make sure nextCookies() is the last plugin in the array
+  plugins: [organization(), admin(), nextCookies()], // make sure nextCookies() is the last plugin in the array
   session: { expiresIn: 60 * 60 },
 });
 
